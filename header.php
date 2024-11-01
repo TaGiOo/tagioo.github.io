45295ebeebcc55400bed9abb37d09258ab19f089
TaGiOo <alan5.siviniant@gmail.com>
2024-11-01 13:32:27 +0100

    gros nettoyage

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="keywords" content="Game Design, Level Design, Portfolio">
+  <meta name="author" content="Alan Siviniant">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="shortcut icon" href="img/favicon.png">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<style>
+  @font-face {
+      font-family:'Helvetica';
+      src: url(fonts/helve/Helvetica-Bold.ttf);
+  }
+  @font-face {
+      font-family: 'Aktiv Grotesk Light';
+      src: url(fonts/aktiv/AktivGroteskCorp-Light.ttf);
+  }
+  @font-face {
+      font-family: 'Aktiv Grotesk Medium';
+      src: url(fonts/aktiv/AktivGroteskCorp-Medium.ttf);
+  }
+</style>
+
+<body <?php body_class(); ?>>
+
+  <header class="header">
+
+    <a href="<?php echo home_url( '/' ); ?>"><h1>ALAN SIVINIANT</h1></a>
+
+    <ul class="menu">
+        
+    <?php wp_nav_menu( array( 'theme_location' => 'main' ) ); ?>
+
+    </ul>
+
+    <ul class="reseaux">
+        <a href="https://www.youtube.com/channel/UCMJFtlKDMVaQC2K7ShBegbg" class="scale"><li><img src="<?php echo get_template_directory_uri(); ?>/img/youtube.png" width="30px"></li></a>
+        <a href="https://www.linkedin.com/in/alan-siviniant-a31267202/" class="scale"><li><img src="<?php echo get_template_directory_uri(); ?>/img/linkedin.png" width="30px"></li></a>
+        <a href="https://alans-tagioo.itch.io" class="scale"><li><img src="<?php echo get_template_directory_uri(); ?>/img/itch-io.png" width="30px"></li></a>
+        <a href="<?php echo get_template_directory_uri(); ?>/img/Alan Siviniant CV.pdf" ><li id="cv"><p id="moncv">Mon CV</p></li></a>
+    </ul>
+
+  </header>
--- wp-content/themes/alans/index.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-
-<h1>Whoops... ! How did you get there ?</h1>
-
-<?php get_footer(); ?>
