61ff4411dafbd1c8d21219fdba969b8ebd93d87c
TaGiOo <alan5.siviniant@gmail.com>
2024-11-01 14:34:27 +0100

    hayaku ?

--- footer.php
+++
@@ -1,9 +0,0 @@
-
-
-    <footer>
-        <a href="<?php echo home_url( '/' ); ?>" class="scale">Politique de confidentialité</a>
-        <a class="scale" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Mentions Légales</a>
-        <a href="<?php echo home_url( '/' ); ?>" class="scale">Website by Alan Siviniant</a>
-    </footer>
-</body>
-</html>
